4ce10d3f1aa346e9d0967391ceb3132f8974fa2d
Arman Borhan <91340628+Fennec-3@users.noreply.github.com>
2021-09-28 11:41:06 -0600

    Rotation Demo

---
+++ Rotation-Demo/sketch.js
@@ -0,0 +1,20 @@
+// Rotation Demo
+
+let x, y;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  x = 0;
+  y = height;
+}
+
+function draw() {
+  background(220);
+
+  translate(x, y);
+  let theta = atan2(mouseY-y, mouseX-x);
+  rotate(theta);
+  fill("black");
+  rect(0, 0, 200, 50);
+}
