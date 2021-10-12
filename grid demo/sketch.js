84c73b6a4905afb458cae9a1022c428375808126
Arman Borhan <91340628+Fennec-3@users.noreply.github.com>
2021-10-12 12:13:56 -0600

    grid toggle

--- grid demo/sketch.js
+++ grid demo/sketch.js
@@ -1,6 +1,6 @@
 //Grid Demo
 
-let gridSize = 400;
+let gridSize = 50;
 let grid;
 
 function setup() {
@@ -11,6 +11,21 @@ function setup() {
 function draw() {
   background(220);
   displayGrid();
+}
+
+function mousePressed() {
+  let cellWidth = width/gridSize;
+  let cellHeight = height/gridSize;
+
+  let cellX = Math.floor(mouseX/cellWidth);
+  let cellY = Math.floor(mouseY/cellHeight);
+
+  if (grid[cellY][cellX] === 1) {
+    grid[cellY][cellX] = 0;
+  }
+  else if (grid[cellY][cellX] === 0) {
+    grid[cellY][cellX] = 1;
+  }
 }
 
 function displayGrid() {
@@ -35,11 +50,12 @@ function createEmpty2DArray(rows, cols) {
   for (let y=0; y<rows; y++) {
     grid.push([]);
     for (let x=0; x<cols; x++) {
-      if (random(100) < 50) {
-        grid[y].push(0);
-      } else {
-        grid[y].push(1);
-      }
+      // if (random(100) < 50) {
+      //   grid[y].push(0);
+      // } else {
+      //   grid[y].push(1);
+      // }
+      grid[y].push(0);
     }
   }
   return grid;
