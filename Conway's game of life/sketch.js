2dabd1335319745c7d5eab9cb13edd8f49f79f34
Arman Borhan <91340628+Fennec-3@users.noreply.github.com>
2021-10-14 12:11:08 -0600

    game of life started

---
+++ Conway's game of life/sketch.js
@@ -0,0 +1,17 @@
+//Game of Life
+
+let grid;
+let gridSize = 10;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+
+function createEmpty2DArray(rows, cols) {
+
+}
