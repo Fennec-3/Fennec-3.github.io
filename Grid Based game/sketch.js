b3642fced34318f3d7e840dcdb051731017cfc93
Arman Borhan <91340628+Fennec-3@users.noreply.github.com>
2021-10-26 12:14:09 -0600

    Minesweeper debugging

--- Grid Based game/sketch.js
+++ Grid Based game/sketch.js
@@ -5,6 +5,9 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// To-Do: make it so the number of bombs = 10% of the squares on the grid,
+// auto fill, detonation sequence, make white tiles display number of nearby bombs
+
 let array;
 let sqWidth = 50;
 let numHeight, numWidth;
@@ -16,6 +19,14 @@ function setup() {
   numHeight = floor(height/sqWidth);
 
   array = createGrid(numHeight, numWidth);
+  for (let i=0; i<array.length; i++) {
+    for (let j=0; j<array[i].length; j++) {
+      if (array[i][j] === 2) {
+        bombCount++;
+      }
+    }
+  }
+  console.log(bombCount);
   console.log(array);
 }
 
@@ -29,12 +40,7 @@ function createGrid(numHeight, numWidth) {
   for (let y=0; y<numHeight; y++) {
     board.push([]);
     for (let x=0; x<numWidth; x++) {
-        if (bombCount <= floor((numHeight*numWidth)/10)) {
-          board[y].push(floor(random(10)));
-          if (board[y][x] === 2) {
-            bombCount++;
-        }
-      }
+      board[y].push(floor(random(2, 10)));
     }
   }
   return board;
@@ -43,12 +49,12 @@ function createGrid(numHeight, numWidth) {
 function displayArray() {
   for (let y=0; y<numHeight; y++) {
     for (let x=0; x<numWidth; x++) {
-      if (array[y][x] !== 2) {
-       fill(220);
-      } else if (array[y][x] === 1) {
-        fill(255);
+      if (array[y][x] === 1) {
+       fill(255);
       } else if (array[y][x] === 10) {
         fill("red");
+      } else {
+        fill(220);
       }
       rect(x*sqWidth, y*sqWidth, sqWidth, sqWidth);
     }
@@ -59,10 +65,12 @@ function mousePressed() {
   let sqX = floor(mouseX/sqWidth);
   let sqY = floor(mouseY/sqWidth);
 
-  if (array[sqY][sqX] !== 2) { 
+  if (array[sqY][sqX] !== 2 && array[sqY][sqX] !== 10) { 
     array[sqY][sqX] = 1;
   }
   else if (array[sqY][sqX] === 2) {
     array[sqY][sqX] = 10; //activated bomb
+
+    //add lose code here
   }
 }
