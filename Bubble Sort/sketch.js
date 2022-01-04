90dfc46ec58109cdd6df723028ff988b763a5f23
Arman Borhan <91340628+Fennec-3@users.noreply.github.com>
2022-01-04 11:55:43 -0600

    Sorting Complete

--- Bubble Sort/sketch.js
+++ Bubble Sort/sketch.js
@@ -1,13 +1,19 @@
 // Bubble Sort
 
-let someList = [5, 15, 3, 8, 9, 1, 20, 7];
+let someList = [17, 8, 12, 4, 16, 3, 20, 11, 1];
 
 function bubbleSort(aList) {
-  for (let i=0; i<aList.length; i++) {
-    if(aList[i] > aList[i+1]) {
-      let monke = aList[i];
-      aList[i] = aList[i+1];
-      aList[i+1] = monke;
+  let anySwaps = true;
+  while(anySwaps) {
+    anySwaps = false;
+
+    for (let i=0; i<aList.length-1; i++) {
+      if(aList[i] > aList[i+1]) {
+        anySwaps = true;
+        let monke = aList[i];
+        aList[i] = aList[i+1];
+        aList[i+1] = monke;
+      }
     }
   }
   return aList;
