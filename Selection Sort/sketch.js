c0dbabf2411214e53321ff80ebd9a27ca9ef9d27
Arman Borhan <91340628+Fennec-3@users.noreply.github.com>
2022-01-07 18:05:02 -0600

    started doing recursive solution
    I'll finish it later im sad rn

--- Selection Sort/sketch.js
+++ Selection Sort/sketch.js
@@ -2,30 +2,46 @@
 
 let someList = [5, 15, 3, 8, 9, 1, 20, 7];
 
-function selectionSort(aList) {
-  for (let x=0; x<someList.length; x++) {
-    let highestNum = 0;
-    for (let i=0; i<aList.length-1; i++) {
-      let endPoint = aList.length-1;
-      if (aList[i] > aList[highestNum]) {
-        highestNum = i;
-      }
-      if (i === endPoint) {
-        let monke = aList[endPoint];
-        aList[endPoint] = aList[highestNum];
-        aList[highestNum] = monke;
-        endPoint--;
-      }
+function selectionSort(newList, oldList, w) {
+  if (newList === oldList) {
+    return newList;
+  }
+
+  let highNum = 0;
+  let swapNum = newList.length-1-w;
+  for (let i=0; i<newList.length-1; i++) {
+    if (newList[i] > newList[highNum]) {
+      highNum = i;
+    } else if (i === swapNum) {
+      
     }
   }
-  return aList;
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  console.log(selectionSort(someList));
+  console.log(selectionSort(someList, [], 0));
 }
 
 function draw() {
   background(220);
+}
+
+function noRecursion() {
+  // for (let x=0; x<someList.length; x++) {
+  //   let highestNum = 0;
+  //   for (let i=0; i<aList.length-1; i++) {
+  //     let endPoint = aList.length-1;
+  //     if (aList[i] > aList[highestNum]) {
+  //       highestNum = i;
+  //     }
+  //     if (i === endPoint) {
+  //       let monke = aList[endPoint];
+  //       aList[endPoint] = aList[highestNum];
+  //       aList[highestNum] = monke;
+  //       endPoint--;
+  //     }
+  //   }
+  // }
+  // return aList;
 }
