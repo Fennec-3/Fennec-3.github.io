db04d3888bae19e1b1ab9edcf41c803641c8d59b
Arman Borhan <91340628+Fennec-3@users.noreply.github.com>
2022-01-09 18:29:06 -0600

    did it without recursion
    needed help from cs30 tho

--- Selection Sort/sketch.js
+++ Selection Sort/sketch.js
@@ -2,52 +2,56 @@
 
 let someList = [5, 15, 3, 8, 9, 1, 20, 7];
 
-function selectionSort(newList, oldList, w) {
-  if (newList === oldList) {
-    return newList;
-  } else {
-    oldList = newList;
-  }
+// function selectionSort(newList, oldList, w) {
+//   if (newList === oldList) {
+//     return newList;
+//   } else {
+//     oldList = newList;
+//   }
 
-  let highNum = 0;
-  let swapNum = newList.length-1-w;
-  for (let i=0; i<newList.length-1; i++) {
-    if (newList[i] > newList[highNum]) {
-      highNum = i;
-    } 
-    if (i === swapNum) {
-      let monke = newList[swapNum];
-      newList[swapNum] = newList[highNum];
-      newList[highNum] = monke;
-      selectionSort(newList, oldList, w++);
-    }
-  }
-}
+//   let highNum = 0;
+//   let swapNum = newList.length-1-w;
+//   for (let i=0; i<newList.length-1; i++) {
+//     if (newList[i] > newList[highNum]) {
+//       highNum = i;
+//     } 
+//     if (i === swapNum) {
+//       let monke = newList[swapNum];
+//       newList[swapNum] = newList[highNum];
+//       newList[highNum] = monke;
+//       selectionSort(newList, oldList, w++);
+//     }
+//   }
+// }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  console.log(selectionSort(someList, [], 0));
+  console.log(noRecursion(someList));
 }
 
 function draw() {
   background(220);
 }
 
-function noRecursion() {
-  // for (let x=0; x<someList.length; x++) {
-  //   let highestNum = 0;
-  //   for (let i=0; i<aList.length-1; i++) {
-  //     let endPoint = aList.length-1;
-  //     if (aList[i] > aList[highestNum]) {
-  //       highestNum = i;
-  //     }
-  //     if (i === endPoint) {
-  //       let monke = aList[endPoint];
-  //       aList[endPoint] = aList[highestNum];
-  //       aList[highestNum] = monke;
-  //       endPoint--;
-  //     }
-  //   }
-  // }
-  // return aList;
+function noRecursion(aList) {
+  let swapLocation = aList.length-1;
+
+  while(swapLocation > 0) {
+    let highestLocation = 0;
+
+    //one pass
+    for (let current=0; current<=swapLocation; current++) {
+      if (aList[current] > aList[highestLocation]) {
+        highestLocation = current;
+      }
+    }
+
+    //swap
+    let monke = aList[swapLocation];
+    aList[swapLocation] = aList[highestLocation];
+    aList[highestLocation] = monke;
+    swapLocation--;
+    console.log(aList);
+  }
+  return aList;
 }
