ed4bd2e3f5d3f30bf7ab008bc073d127d6074133
Arman Borhan <91340628+Fennec-3@users.noreply.github.com>
2022-01-24 12:26:00 -0600

    declan Beta test
    gfhdjskl;

---
+++ Declan-template/sketch.js
@@ -0,0 +1,37 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let ballX = width/2;
+let ballY = height/2;
+let r = 25;
+let dx = 5;
+let dy = 3;
+
+function preload() {
+  font = loadFont("./assets/CONSOLA.TTF");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  background(0, 0);
+  fill(255);
+
+  charSetup(12);
+  gradientStyle(2);
+}
+
+function draw() {
+  
+
+  printOut();
+}
+
+function displayBall() {
+  if (x<width)
+}
+
