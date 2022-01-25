30dade8c10fd5219b3d8884a58ba804f81ce69ec
Arman Borhan <91340628+Fennec-3@users.noreply.github.com>
2022-01-24 20:50:20 -0600

    doesn't work

--- Declan-template/sketch.js
+++ Declan-template/sketch.js
@@ -5,11 +5,13 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let ballX = width/2;
-let ballY = height/2;
+let ballX;
+let ballY;
 let r = 25;
 let dx = 5;
 let dy = 3;
+let ballFill;
+let strokeFill;
 
 function preload() {
   font = loadFont("./assets/CONSOLA.TTF");
@@ -17,6 +19,11 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  ballX = width/2;
+  ballY = height/2;
+
+  ballFill = randChar();
+  strokeFill = randChar();
 
   background(0, 0);
   fill(255);
@@ -26,12 +33,31 @@ function setup() {
 }
 
 function draw() {
-  
+  moveBall();
+  displayBall();
 
   printOut();
 }
 
 function displayBall() {
-  if (x<width)
+  charFill(ballFill);
+  charStroke(strokeFill);
+  charCircle(ballX, ballY, r*2);
 }
 
+function moveBall() {
+  ballX += dx;
+  ballY += dy;
+
+  if (ballX + r >= width || ballX - r <= 0) {
+    dx = -dx;
+    ballFill = randChar();
+    strokeFill = randChar();
+  }
+
+  if (ballY + r >= height || ballY - r <= 0) {
+    dy = -dy;
+    ballFill = randChar();
+    strokeFill = randChar();
+  }
+}
---
+++ declans-new-template/sketch.js
@@ -0,0 +1,19 @@
+function preload() {
+  font = loadFont("./assets/CONSOLA.TTF");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  background(0, 0);
+  fill(255);
+
+  charSetup(12);
+  gradientStyle(0);
+}
+
+function draw() {
+  // your code goes here!
+
+  printOut();
+}
